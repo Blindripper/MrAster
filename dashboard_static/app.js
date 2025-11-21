83199579124f48cdd5481af8f0777b68de4242a1
Blindripper <39067706+Blindripper@users.noreply.github.com>
2025-11-21 18:50:41 +0100

    Hide active positions card when empty

--- dashboard_static/app.js
+++ dashboard_static/app.js
@@ -7234,8 +7234,10 @@ function updateActivePositionsView(options = {}) {
   if (activePositionsCard) {
     if (hasRows) {
       activePositionsCard.removeAttribute('data-empty');
+      activePositionsCard.removeAttribute('hidden');
     } else {
       activePositionsCard.setAttribute('data-empty', 'true');
+      activePositionsCard.setAttribute('hidden', 'true');
     }
   }
 
